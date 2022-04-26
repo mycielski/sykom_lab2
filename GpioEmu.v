82bdc4e8076ac7f8d09c0116b1e5e933f5659906
dkisiel <01159016@pw.edu.pl>
2022-04-26 18:08:40 +0200

    KURWADZIALA!!!!1!!!

--- GpioEmu.v
+++ GpioEmu.v
@@ -1,4 +1,4 @@
-module gpioemu(n_reset, saddress[15:0], srd, swr, sdata_in[31:0], sdata_out[31:0], gpio_in[31:0], gpio_latch, gpio_out[31:0], clk, gpio_in_s_insp[31:0]);
+module GpioEmu(n_reset, saddress[15:0], srd, swr, sdata_in[31:0], sdata_out[31:0], gpio_in[31:0], gpio_latch, gpio_out[31:0], clk, gpio_in_s_insp[31:0]);
 
 input n_reset;
 input [15:0] saddress;
@@ -15,12 +15,11 @@ output [31:0] gpio_in_s_insp;
 
 reg [31:0] gpio_in_s;
 reg [31:0] gpio_out_s;
-reg [31:0] sdata_in_s;
 reg [31:0] sdata_out_s;
 
 
 
-//odpowiedz na reset (aktywowane przejściem 1->0)
+//Reset
 always@(negedge n_reset) begin
     sdata_out_s <= 0;
     gpio_out_s <= 0;
@@ -28,36 +27,38 @@ always@(negedge n_reset) begin
 end
 
 // odczyt z portu GPIO
-always@(posedge gpio_in) begin
+always@(posedge gpio_latch) begin
     gpio_in_s <= gpio_in;
 end
 
 // odczyt na gpio
 
 wire [11:0] cut_address;
-assign cut_address = {sadddress[15:12], sadddress[7:0]};
+assign cut_address = {saddress[15:8], saddress[3:0]};
 
 always@(posedge srd) begin
     if(cut_address == 12'h6b0)
-        sdata_in_s <= gpio_in_s[3:0] << 8;
+        //sdata_out_s <= gpio_in_s[3:0] << 8;
+        sdata_out_s <= (gpio_in_s & 32'h0f) << 8;
     else if (cut_address == 12'hdb0) 
-        sdata_in_s <= gpio_in_s[7:4] << 20;
-    else 
-        sdata_in_s <= 0;
+        sdata_out_s <= (gpio_in_s & 32'h0f) << 20;
+    // else 
+    //     sdata_out_s <= 0;
 end
 
 
 always@(posedge swr) begin
     if(cut_address == 12'h6b0)
-        sdata_out_s[3:0] <= sdata_out[11:8];
+        gpio_out_s[3:0] <= sdata_in[11:8];  
     else if (cut_address == 12'hdb0) 
-        sdata_out_s[7:4] <= sdata_out[23:20];
-    else 
-        gpio_out_s <= gpio_out;
+        gpio_out_s[7:4] <= sdata_in[23:20];  
+    // else 
+    //     gpio_out_s <= gpio_out;
 end
 
-assign sdata_in = sdata_in_s;
+assign sdata_out = sdata_out_s;
 assign gpio_out = gpio_out_s;
+assign gpio_in_s_insp = gpio_in_s;
 
 
 endmodule
--- GpioEmu_tb.v
+++ GpioEmu_tb.v
@@ -1,15 +1,87 @@
+`timescale 1 ns /10 ps
+
 module GpioEmu_tb;
-initial begin
-$dumpfile("GpioEmu.vcd");
-$dumpvars(0, GpioEmu_tb);
-End
-initial begin
-# 5 reset = 0;
-# 10 sadddress = ...;
-# 11 sdata = ...;
-# 12 swr=...;
-...
-# 1000 $finish;
-end
-...
+
+	reg  n_reset = 1;
+	reg[15:0] saddress = 0;
+	reg[31:0] sdata_in = 0;
+	reg[31:0] gpio_in = 0;
+	reg gpio_latch = 0;
+	reg clk= 0;
+	reg srd = 0;
+	reg swr = 0;
+
+	wire[31:0] gpio_out_test;
+	wire[31:0] sdata_out_test;
+	wire[31:0] gpio_in_s_insp_test;
+
+	initial begin
+		$dumpfile("GpioEmu.vcd");
+		$dumpvars(0,GpioEmu_tb);
+	end
+	
+	always
+		#1 clk= ~clk;
+
+    initial begin 
+
+        //reset
+        # 5 n_reset = 0;
+        # 5 n_reset = 1;
+
+        //zapis 1 oś
+        #5 saddress= 16'h6ba0;
+		#5 sdata_in = 32'h123fffaa;
+		#5 swr= 1;
+		#5 swr= 0;
+
+        //zapis 2 oś
+        #5 saddress= 16'hdb10;
+		#5 sdata_in = 32'h123fffaa;
+		#5 swr= 1;
+		#5 swr= 0;
+
+        //zły zapis
+        #5 saddress= 16'hffff;
+		#5 sdata_in = 32'h123fffaa;
+		#5 swr= 1;
+		#5 swr= 0;
+
+        //reset
+        # 5 n_reset = 0;
+        # 5 n_reset = 1;
+
+        //odczyt z osi 1
+		#5 gpio_in = 32'h123fffaa;
+		#5 gpio_latch = 1;
+		#5 gpio_latch = 0;
+		#5 saddress= 16'h6ba0;
+		#5 srd= 1;
+		#5 srd= 0;
+		
+		//odczyt z osi 2
+		#5 gpio_in = 32'h123fffaa;
+		#5 gpio_latch = 1;
+		#5 gpio_latch = 0;
+		#5 saddress= 16'hdb10;;
+		#5 srd= 1;
+		#5 srd= 0;
+
+		//odczyt ze złego adresu
+		#5 gpio_in = 32'h123fffaa;
+		#5 gpio_latch = 1;
+		#5 gpio_latch = 0;
+		#5 saddress= 16'hffff;
+		#5 srd= 1;
+		#5 srd= 0;
+
+		//reset
+		#5 n_reset = 0;
+		#5 n_reset = 1;
+
+		#300 srd= 1;
+		#1500 $finish;
+
+	end
+	GpioEmu test1(n_reset, saddress, srd, swr, sdata_in, sdata_out_test, gpio_in, gpio_latch, gpio_out_test, clk, gpio_in_s_insp_test);
 endmodule
