e3b98f314a0c1304bd759064d6710ee31bb8e7ef
Tomasz Mycielski <76878490+mycielski@users.noreply.github.com>
2022-05-22 22:26:42 +0200

    Update and rename GpioEmu.v to gpioemu.v

--- gpioemu.v
+++ gpioemu.v
@@ -1,4 +1,7 @@
-module GpioEmu(n_reset, saddress[15:0], srd, swr, sdata_in[31:0], sdata_out[31:0], gpio_in[31:0], gpio_latch, gpio_out[31:0], clk, gpio_in_s_insp[31:0], counter[7:0]);
+/*verilator lint_off UNUSED */
+/*verilator lint_off WIDTH */
+/*verilator lint_off MULTIDRIVEN */
+module gpioemu(n_reset, saddress[15:0], srd, swr, sdata_in[31:0], sdata_out[31:0], gpio_in[31:0], gpio_latch, gpio_out[31:0], clk, gpio_in_s_insp[31:0], counter[7:0]);
 
 input n_reset;
 input [15:0] saddress;
@@ -19,7 +22,7 @@ reg [31:0] gpio_out_s;
 reg [31:0] sdata_out_s;
 reg [7:0] counter_s;
 //reg count_flag;
-reg int;
+reg interrupt;
 
 
 //Reset
@@ -28,7 +31,7 @@ always@(negedge n_reset) begin
     gpio_out_s <= 0;
     gpio_in_s <= 0;
     counter_s <= 8'h4e;
-    int <= 0;
+    interrupt <= 0;
 end
 
 // odczyt na gpio
@@ -41,7 +44,7 @@ always @(posedge clk) begin
     if(counter_s >= 0)
         counter_s = counter_s - 1;
     else
-        int <= 1;
+        interrupt <= 1;
     //end
 end
 
@@ -55,7 +58,7 @@ always@(posedge srd) begin
         sdata_out_s <= (gpio_in_s & 32'h0f) << 8;
     else if (cut_address == 12'hdb0) 
         sdata_out_s <= (gpio_in_s & 32'h0f) << 20;
-        sdata_out_s <= (int << 4);
+        sdata_out_s <= (interrupt << 4);
     // else 
     //     sdata_out_s <= 0;
 end
